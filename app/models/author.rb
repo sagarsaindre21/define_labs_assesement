b8ee0324120c0957067b311f2926dfe40f6a0d0a
sagar <sagarsaindre1@gmail.com>
2022-02-06 20:58:44 +0530

    Added Birthhdate validation in Author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -5,8 +5,19 @@ class Author < ApplicationRecord
   belongs_to :user
 
   validates :firstname, :lastname, :birthdate, presence: true
+  validate :birth_date_cannot_be_in_the_future
+
+  def birth_date_cannot_be_in_the_future
+    if birthdate.present? && birthdate > Date.today
+      errors.add(:birthdate, "can't be in the future")
+    end
+  end 
 
   def full_name
     "#{self.firstname} #{self.lastname}"    	
-  end  
+  end 
+
+  def display_birthdate
+     self.birthdate.strftime("%d/%m/%Y")
+   end 
 end
